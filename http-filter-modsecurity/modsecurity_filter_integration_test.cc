37b53058012b656da6be9647ba2db150cd9c86f0
Your Name <you@example.com>
2024-02-25 17:29:18 +0100

    Filter test changes

--- http-filter-modsecurity/modsecurity_filter_integration_test.cc
+++ http-filter-modsecurity/modsecurity_filter_integration_test.cc
@@ -1,44 +1,51 @@
 #include "test/integration/http_integration.h"
 #include "test/integration/utility.h"
 
-namespace Envoy {
-class HttpFilterSampleIntegrationTest : public HttpIntegrationTest,
-                                        public testing::TestWithParam<Network::Address::IpVersion> {
-public:
-  HttpFilterSampleIntegrationTest()
-      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
-  /**
-   * Initializer for an individual integration test.
-   */
-  void SetUp() override { initialize(); }
+namespace Envoy
+{
+  class HttpFilterSampleIntegrationTest : public HttpIntegrationTest,
+                                          public testing::TestWithParam<Network::Address::IpVersion>
+  {
+  public:
+    HttpFilterSampleIntegrationTest()
+        : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
+    /**
+     * Initializer for an individual integration test.
+     */
+    void SetUp() override { initialize(); }
 
-  void initialize() override {
-    config_helper_.addFilter("{ name: sample, config: { key: via, val: sample-filter } }");
-    HttpIntegrationTest::initialize();
+    void initialize() override
+    {
+      config_helper_.addFilter("{ name: sample, config: { key: via, val: sample-filter } }");
+      HttpIntegrationTest::initialize();
+    }
+  };
+
+  INSTANTIATE_TEST_SUITE_P(IpVersions, HttpFilterSampleIntegrationTest,
+                           testing::ValuesIn(TestEnvironment::getIpVersionsForTest()));
+
+  TEST_P(HttpFilterSampleIntegrationTest, Test1)
+  {
+    Http::TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/"}, {":authority", "host"}};
+
+    IntegrationCodecClientPtr codec_client;
+    FakeHttpConnectionPtr fake_upstream_connection;
+    IntegrationStreamDecoderPtr response(new IntegrationStreamDecoder(*dispatcher_));
+    FakeStreamPtr request_stream;
+
+    codec_client = makeHttpConnection(lookupPort("http"));
+    // codec_client->makeHeaderOnlyRequest(headers, *response);
+    response = codec_client->makeHeaderOnlyRequest(headers);
+    // fake_upstream_connection = fake_upstreams_[0]->waitForHttpConnection(*dispatcher_);
+    ASSERT_TRUE(fake_upstreams_[0]->waitForHttpConnection(*dispatcher_, fake_upstream_connection));
+    // request_stream = fake_upstream_connection->waitForNewStream(*dispatcher_);
+    ASSERT_TRUE(fake_upstream_connection->waitForNewStream(*dispatcher_, request_stream));
+    ASSERT_TRUE(request_stream->waitForEndStream(*dispatcher_));
+    ASSERT_TRUE(response->waitForEndStream());
+
+    EXPECT_EQ("sample-filter",
+              request_stream->headers().get(Http::LowerCaseString("via"))[0]->value().getStringView());
+
+    codec_client->close();
   }
-};
-
-INSTANTIATE_TEST_SUITE_P(IpVersions, HttpFilterSampleIntegrationTest,
-                        testing::ValuesIn(TestEnvironment::getIpVersionsForTest()));
-
-TEST_P(HttpFilterSampleIntegrationTest, Test1) {
-  Http::TestHeaderMapImpl headers{{":method", "GET"}, {":path", "/"}, {":authority", "host"}};
-
-  IntegrationCodecClientPtr codec_client;
-  FakeHttpConnectionPtr fake_upstream_connection;
-  IntegrationStreamDecoderPtr response(new IntegrationStreamDecoder(*dispatcher_));
-  FakeStreamPtr request_stream;
-
-  codec_client = makeHttpConnection(lookupPort("http"));
-  codec_client->makeHeaderOnlyRequest(headers, *response);
-  fake_upstream_connection = fake_upstreams_[0]->waitForHttpConnection(*dispatcher_);
-  request_stream = fake_upstream_connection->waitForNewStream(*dispatcher_);
-  request_stream->waitForEndStream(*dispatcher_);
-  response->waitForEndStream();
-
-  EXPECT_STREQ("sample-filter",
-               request_stream->headers().get(Http::LowerCaseString("via"))->value().c_str());
-
-  codec_client->close();
-}
 } // namespace Envoy
